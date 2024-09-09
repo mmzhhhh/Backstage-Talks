f672cc1dede8715ee261feb7b6add4fd03d4e965
mmzhhhh <254995030@QQ.com>
2024-09-09 14:37:52 +0800

    Fixed the footer tag position on mobile devices, from fixed -> fluid

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ import logo from "../assests/images/logo.png";
 
 export default function Header() {
   return (
-    <header className=" fixed top-4 left-4 w-[160px] md:w-[260px]">
+    <header className=" fixed top-4 left-4 w-[160px] md:w-[260px] z-50">
       <img
         src={logo}
         alt="Backstage Talks"
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,5 +1,5 @@
 import issue8 from "../assests/images/backstagetalks_cover_issue_8.png";
-import issue7 from "../assests/images/backstagetalks_cover_issue_7.png"
+import issue7 from "../assests/images/backstagetalks_cover_issue_7.png";
 import issue6 from "../assests/images/backstagetalks_cover_issue_6.png";
 import issue5 from "../assests/images/backstagetalks_cover_issue_5.png";
 import issue4 from "../assests/images/backstagetalks_cover_issue_4.png";
@@ -12,7 +12,7 @@ import ReactFullpage from "@fullpage/react-fullpage";
 
 export default function Main({ onColorChange }) {
   const [activeIssue, setActiveIssue] = useState("issue8");
-  
+
   const handleLeave = useCallback(
     (origin, destination, direction) => {
       onColorChange(destination.index);
@@ -26,7 +26,16 @@ export default function Main({ onColorChange }) {
       <ReactFullpage
         scrollingSpeed={1000}
         onLeave={handleLeave}
-        anchors={["issue8", "issue7", "issue6", "issue5", "issue4", "issue3", "issue2", "issue1"]}
+        anchors={[
+          "issue8",
+          "issue7",
+          "issue6",
+          "issue5",
+          "issue4",
+          "issue3",
+          "issue2",
+          "issue1",
+        ]}
         render={() => {
           return (
             <ReactFullpage.Wrapper>
@@ -93,25 +102,102 @@ export default function Main({ onColorChange }) {
                     "https://brot.sk/products/backstage-talks-2?_pos=1&_sid=1c5730cc8&_ss=r"
                   }
                 />
-                
+                <div className="md:hidden">
+                <div className=" md:hidden items-center justify-center text-center w-full">
+                  <p className=" font-bold text-lg">
+                    Backstage Talks is a magazine of casual, but in depth
+                    dialogues on design and business. Our decisions shape and
+                    influence this complex world—to have a chance to make the
+                    right ones, we need to talk.
+                  </p>
+                  <small className=" text-sm">
+                    &copy;&nbsp;2024&nbsp;
+                    <a
+                      href="https://milk.sk/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className=" underline hover:no-underline"
+                    >
+                      Published by studio Milk
+                    </a>
+                  </small>
+                  <br />
+                  <br />
+                  <p className=" mb-4 underline font-bold text-md">
+                    <a href="https://backstagetalks.com/privacy-policy.php">
+                      Privacy Policy
+                    </a>
+                  </p>
+                  {/* contact */}
+                  <p className="mt-4 text-md font-bold underline">
+                    <a href="mailto:info@backstagetalks.com">
+                      info@backstagetalks.com
+                    </a>
+                  </p>
+                </div>
+              </div>
+              </div>
+              <div className="md:hidden">
+                <div className=" md:hidden items-center justify-center text-center max-w-[400px]">
+                  <p className=" font-bold text-lg">
+                    Backstage Talks is a magazine of casual, but in depth
+                    dialogues on design and business. Our decisions shape and
+                    influence this complex world—to have a chance to make the
+                    right ones, we need to talk.
+                  </p>
+                  <small className=" text-sm">
+                    &copy;&nbsp;2024&nbsp;
+                    <a
+                      href="https://milk.sk/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className=" underline hover:no-underline"
+                    >
+                      Published by studio Milk
+                    </a>
+                  </small>
+                  <br />
+                  <br />
+                  <p className=" mb-4 underline font-bold text-md">
+                    <a href="https://backstagetalks.com/privacy-policy.php">
+                      Privacy Policy
+                    </a>
+                  </p>
+                  {/* contact */}
+                  <p className="text-md font-bold underline">
+                    <a href="mailto:info@backstagetalks.com">
+                      info@backstagetalks.com
+                    </a>
+                  </p>
+                </div>
               </div>
             </ReactFullpage.Wrapper>
           );
         }}
       />
-      <ul id="menu" className="hidden md:block md:fixed right-4 bottom-4 text-sm leading-6">
+      <ul
+        id="menu"
+        className="hidden md:block md:fixed right-4 bottom-4 text-sm leading-6"
+      >
         {[
-          {anchor:"issue8", label:"Issue #8"},
-          {anchor:"issue7", label:"Issue #7"},
-          {anchor:"issue6", label:"Issue #6"},
-          {anchor:"issue5", label:"Issue #5"},
-          {anchor:"issue4", label:"Issue #4"},
-          {anchor:"issue3", label:"Issue #3"},
-          {anchor:"issue2", label:"Issue #2"},
-          {anchor:"issue1", label:"Issue #1"},
+          { anchor: "issue8", label: "Issue #8" },
+          { anchor: "issue7", label: "Issue #7" },
+          { anchor: "issue6", label: "Issue #6" },
+          { anchor: "issue5", label: "Issue #5" },
+          { anchor: "issue4", label: "Issue #4" },
+          { anchor: "issue3", label: "Issue #3" },
+          { anchor: "issue2", label: "Issue #2" },
+          { anchor: "issue1", label: "Issue #1" },
         ].map((item) => (
           <li key={item.anchor} data-menuanchor={item.anchor}>
-            <a href={`#${item.anchor}`} className={`hover:underline ${activeIssue===item.anchor?"font-bold":""}`}>{item.label}</a>
+            <a
+              href={`#${item.anchor}`}
+              className={`hover:underline ${
+                activeIssue === item.anchor ? "font-bold" : ""
+              }`}
+            >
+              {item.label}
+            </a>
           </li>
         ))}
       </ul>
